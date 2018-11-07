6fa05e37ee8b6bb931af5bc633812e4d489e43bd
Brian Wiborg <baccenfutter@c-base.org>
2018-11-07 21:46:50 +0100

    fix molecule deprecations

--- molecule/default/tests/test_service.py
+++ molecule/default/tests/test_service.py
@@ -6,7 +6,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_service(Service):
-    s = Service("nginx")
+def test_service(host):
+    s = host.service("nginx")
     assert s.is_running
     assert s.is_enabled
--- molecule/default/tests/test_sockets.py
+++ molecule/default/tests/test_sockets.py
@@ -6,9 +6,9 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_socket_http(Socket):
-    assert Socket("tcp://0.0.0.0:80").is_listening
+def test_socket_http(host):
+    assert host.socket("tcp://0.0.0.0:80").is_listening
 
 
-def test_socket_https(Socket):
-    assert Socket("tcp://0.0.0.0:443").is_listening
+def test_socket_https(host):
+    assert host.socket("tcp://0.0.0.0:443").is_listening
